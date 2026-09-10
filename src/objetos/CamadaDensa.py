e8dd835e3f33536fadb5ce4bfd1d570978cc70a4
Vitor-Boldrin <vitor.boldrin7@hotmail.com>
2026-09-10 19:23:35 -0300

    Desenvolvimento camada densa

--- src/objetos/CamadaDensa.py
+++ src/objetos/CamadaDensa.py
@@ -1,6 +1,20 @@
+from funcoes import _sigmoid
+import numpy as np
+
 class CamadaDensa:
-    def __init__(self,numero_camadas):
-        self._numero_camadas = numero_camadas
-        # atributos
+    """
+        Objeto da camada densa de uma rede neural, seu
+        objetivo é receber uma entrada, input da rede ou a 
+        saida de uma outra camada, coletar esses valores
+        multiplicar pelos parâmetros, utilizar uma função
+        de ativação e retornar isso como a saída
+        possui o atributo:
+            parametros (np.array mxn)
+    """
+    def __init__(self,tamanhoEntrada:int,tamanhoSaida:int,funcaoAtivacao='sigmoid'):
+        self._numero_camadas = np.random.rand(tamanhoSaida, tamanhoEntrada)
+
+    def _foward(self,entrada:np.array):
+        
 
     
