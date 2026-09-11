5d8ae52e6d7afa4cfdf7f80390c13c5e88edf0c4
mcaschultz <mcaschultz@hotmail.com>
2026-09-11 00:23:48 -0300

    Implementação do método para separação de conjuntos holdout

--- src/objetos/dados.py
+++ src/objetos/dados.py
@@ -5,7 +5,7 @@ import random
 
 # TODO LIST
 # [X] Ler aquivos .csv
-# [ ] Implementar método holdout
+# [X] Implementar método holdout
 # [ ] Implementar método K-Fold
 # [ ] Implementar método Bootstrap
 
@@ -26,8 +26,12 @@ class Dados():
         self.pacote_dados = self._ler_dados(caminho_dados)
         self.pacote_rotulos = self._ler_dados(caminho_rotulos)
 
-        self.pontos = [[a,b] for a,b in zip(self.pacote_dados, self.pacote_labels)]
-        self.numero_pontos = len(self.pontos)
+        self.conjunto_pontos = [[a,b] for a,b in zip(self.pacote_dados, self.pacote_rotulos)]
+        self.numero_pontos = len(self.conjunto_pontos)
+
+        self.conjunto_treinamento = []
+        self.conjunto_validacao = []
+        self.conjunto_teste = []
 
     def _ler_dados(self, caminho):
         """
@@ -38,13 +42,47 @@ class Dados():
             Retorna:
                 - dados: Numpy array contendo as informações no arquivo .csv. 
         """
-        
+
         try:
             dados = pd.read_csv(caminho, thousands=",").to_numpy()
         except FileNotFoundError:
             print(f"O Arquivo '{caminho}' não foi encontrado.")
         except EmptyDataError:
             print(f"O Arquivo '{caminho}' está vazio.")
+
         return dados
         
+    def _holdout(self, *args):
+
+        self.pontos_embaralhados = self.conjunto_pontos.copy()
+        random.shuffle(self.pontos_embaralhados)
+
+        if len(args) == 0:
+
+            raise ValueError("É necessario ao menos especificar o tamanho do conjunto de treinamento.")
+
+        elif len(args) == 1:
+
+            if args[0] >= 1.0:
+                raise ValueError("O tamanho do conjunto de treinamento não pode ultrapassar ou ser igual ao número de pontos.")
+
+            self.conjunto_treinamento = self.pontos_embaralhados[ : int(np.ceil(self.numero_pontos*args[0]))]
+            self.conjunto_validacao = []
+            self.conjunto_teste = self.pontos_embaralhados[int(np.ceil(self.numero_pontos*args[0])) : ]
+            
+        elif len(args) == 2:
+
+            if args[0] >= 1.0:
+                raise ValueError("O tamanho do conjunto de treinamento não pode ultrapassar ou ser igual ao número de pontos.")
+            elif args[1] >= 1.0:
+                raise ValueError("O tamanho do conjunto de validação não pode ultrapassar ou ser igual ao número de pontos.")
+            elif args[0] + args[1] >= 1.0:
+                raise ValueError("A soma entre o tamanho do conjunto de treinamento e o tamanho do conjunto de validação não pode ultrapassar ou ser igual ao número de pontos.")
+
+            self.conjunto_treinamento = self.pontos_embaralhados[ : int(np.ceil(self.numero_pontos*args[0]))]
+            self.conjunto_validacao = self.pontos_embaralhados[ int(np.ceil(self.numero_pontos*args[0])) : int(np.ceil(self.numero_pontos*args[0])) + int(np.ceil(self.numero_pontos*args[1]))]
+            self.conjunto_teste = self.pontos_embaralhados[int(np.ceil(self.numero_pontos*args[0])) + int(np.ceil(self.numero_pontos*args[1])) : ]
+            
+        else:
+            raise ValueError("Muitos argumentos, era esperado o tamanho do conjunto de treinamento e o tamanho do conjunto de validação.")
 
--- src/test_leitura_dados.py
+++ src/test_leitura_dados.py
@@ -1,6 +1,42 @@
 import objetos
 import numpy as np
 
-dados = objetos.Dados(caminho_dados, caminho_labels)
 
-print(dados._separacao_holdout())
+dados = objetos.Dados(caminho_dados, caminho_labels)
+numero_pontos_totais = len(dados.conjunto_pontos)
+
+print(f"Número de Pontos totais: {numero_pontos_totais}\n")
+
+porcentagem_treinamento = 0.6
+porcentagem_validacao = 0.2
+
+dados._holdout(porcentagem_treinamento, porcentagem_validacao)
+
+print("Tamanho conjunto de treinamento")
+print(len(dados.conjunto_treinamento))
+print("Tamanho conjunto de validação")
+print(len(dados.conjunto_validacao))
+print("Tamanho conjunto de testes")
+print(len(dados.conjunto_teste))
+
+print("Teste para ver se o conjunto é igual a sua parte no conjunto dos pontos embaralhados")
+print(" Conjunto de treinamento")
+print(f"    O conjunto tem elementos? {dados.conjunto_treinamento != []}")
+if dados.conjunto_treinamento != [] :
+    print(f"    É igual? {dados.conjunto_treinamento == dados.pontos_embaralhados[: int(np.ceil(numero_pontos_totais * porcentagem_treinamento))]}")
+
+print(" Conjunto de validação")
+print(f"    O conjunto tem elementos? {dados.conjunto_validacao != []}")
+if dados.conjunto_validacao != [] :
+    print(f"    É igual? {dados.conjunto_validacao == dados.pontos_embaralhados[int(np.ceil(numero_pontos_totais * porcentagem_treinamento)) : int(np.ceil(numero_pontos_totais * porcentagem_treinamento)) +  int(np.ceil(numero_pontos_totais * porcentagem_validacao))]}")
+
+print(" Conjunto de teste")
+print(f"    O conjunto tem elementos? {dados.conjunto_teste != []}")
+
+if dados.conjunto_teste != [] :
+    if dados.conjunto_validacao != [] and dados.conjunto_teste != []:
+        print(f"    É igual? {dados.conjunto_teste == dados.pontos_embaralhados[int(np.ceil(numero_pontos_totais * porcentagem_treinamento)) +  int(np.ceil(numero_pontos_totais * porcentagem_validacao)) : ]}")
+    elif dados.conjunto_teste != []:
+        print(f"    É igual? {dados.conjunto_teste == dados.pontos_embaralhados[int(np.ceil(numero_pontos_totais * porcentagem_treinamento))  : ]}")
+    else:
+        print("ERROR")
